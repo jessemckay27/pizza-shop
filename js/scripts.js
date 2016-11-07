1f140735b874e009d78e1ca77f2740c42c13b558
Jesse <rosecity27@comcast.net>
2016-11-06 17:36:15 -0800

    add prototype that takes prices from object and adds them

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,20 @@ function Order (toppings, sides) {
 	this.sidesChoices = [];
 }
 
+Order.prototype.getPrice = function() {
+	var total = 10;
+	for (i=0; i < this.toppingsChoices.length; i++) {
+		var price = this.toppingsChoices[i];
+		total += price;	
+	}
 
+	for (i=0; i < this.sidesChoices.length; i++) {
+		var price = this.sidesChoices[i];
+		total += price;	
+	}
+
+	return total;
+}
 
 
 $(document).ready(function(){
@@ -14,14 +27,16 @@ $(document).ready(function(){
 		var myOrder = new Order(inputtedToppings, inputtedSides);
 
 		var inputtedToppings = $("input:checkbox[name=toppings]:checked").each(function() {
-			myOrder.toppingsChoices.push($(this).val());
+			myOrder.toppingsChoices.push(parseInt($(this).val()));
+			$("#output-toppings").append();
 		});
 
 		var inputtedSides = $("input:checkbox[name=sides]:checked").each(function() {
-			myOrder.sidesChoices.push($(this).val());
+			myOrder.sidesChoices.push(parseInt($(this).val()));
+			$("#output-sides").append();
 		});
 
-		console.log(myOrder);
+		$("#output-wrap").show();
 
 	});
 });
